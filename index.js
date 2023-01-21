10ac9bde9b6f44f50731218c9ca64fb8400277ec
Nadir Chughtai <nadirchugtai1999@gmail.com>
2023-01-21 11:55:48 +0500

    new changes

---
+++ index.js
@@ -0,0 +1,51 @@
+            //  Stings
+ const x=' This is the first string';
+const y=['This is the Second string',' String 3'];
+const z= x+' '+y ;
+const xyz='This is ,the NEW string';
+// const newStr=x.concat(...y);    // ... Are spread operator 
+//  console.log(newStr);
+//  console.log(x[0]);
+// console.log(x.length);   
+// const sliceString=x.slice(5,10).charAt(0).toUpperCase();
+// const sliceString2=y.slice(1)
+// console.log(sliceString2);
+
+// const part=x.substring(0,10);
+// console.log(part);
+
+// console.log(x.toUpperCase());
+// console.log(x.trim()); //remove whitespaces
+
+// const replaceStr = xyz.replace('This','that');
+// console.log(replaceStr);
+
+// console.log(xyz.split());
+// console.log(xyz.split(' ').join(' '));
+// console.log(...xyz);
+
+
+                //    Logical Operators
+
+const a=10;
+const b=20;
+
+(a<8 && b>1)
+{
+console.log('True')
+} 
+
+ (a==5 || b==5) 
+{
+    console.log('true')
+}
+(!(x==y))
+{
+    console.log('false');
+}
+
+
+
+
+
+
